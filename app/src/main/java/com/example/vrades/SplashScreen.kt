caea266c32408865cb6a5f17f4e989be7c1f8a5d
Alex Madaras <alexandru.madaras00@e-uvt.ro>
2022-02-04 14:33:07 +0200

    feature-added-splash-screen
    -Added the Splash Screen for the application

---
+++ app/src/main/java/com/example/vrades/SplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.vrades
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
